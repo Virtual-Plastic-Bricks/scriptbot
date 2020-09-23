4df7ef24906f7a24f09b1f61869f7ac4019d9f63
Skye <9571697+eLunate@users.noreply.github.com>
2020-09-23 22:00:39 +0000

    I think this is a good start

---
+++ src/bot.rs
@@ -0,0 +1,19 @@
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::prelude::*;
+
+pub struct Bot;
+
+#[async_trait]
+impl EventHandler for Bot {
+    async fn message(&self, _ctx: Context, message: Message) {
+        // Is the message in the question-asking channel?
+        // Yes? Perfect. Check that it's a question. If it's not a question, it's a mini-mute.
+        // Create new channel for Q
+        // Request more information (if needed) and open to public once available
+        // Look for: answer cmd, close cmd, accept cmd
+        // When answer accepted, delete channel (w/ saved results) + open Q chan to user again
+
+        unimplemented!()
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,19 @@
+mod bot;
+
+use serenity::client::bridge::gateway::GatewayIntents;
+use serenity::client::Client;
+
+#[tokio::main]
+async fn main() {
+    let token = std::env::args().nth(1).expect("No token supplied");
+    let mut client = Client::new(token)
+        .event_handler(bot::Bot)
+        .intents(GatewayIntents::GUILD_MESSAGES)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
